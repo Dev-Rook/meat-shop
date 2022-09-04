4a389b2d516b61445f426b495e59a8b47eddd54b
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-09-04 18:24:46 -0500

    Mapped to slide. Styling next.
    Open your mind

--- src/Components/Cards/Chicken.jsx
+++ src/Components/Cards/Chicken.jsx
@@ -3,44 +3,96 @@ const Chicken = [
         id: 1,
         Image: "",
         Title: "1 Chicken",
-        Decription: "",
+        Description: "One whole chicken. Defeathered and packaged",
         Category: "Whole Ckicken",
     },
     {
         id: 2,
         Image: "",
         Title: "Cut-Up",
-        Decription: "",
+        Description: "",
         Category: "Whole Ckicken",
     },
     {
         id: 3,
         Image: "",
         Title: "Minced",
-        Decription: "",
+        Description: "",
         Category: "Whole Ckicken",
     },
     {
         id: 4,
         Image: "",
         Title: "Legs",
-        Decription: "",
+        Description: "",
         Category: "Part",
     },
     {
         id: 5,
         Image: "",
         Title: "Wings",
-        Decription: "",
+        Description: "",
         Category: "Part",
     },
     {
         id: 6,
         Image: "",
         Title: "Thighs",
-        Decription: "",
+        Description: "",
         Category: "Part",
     },
 ]
 
 export default Chicken
+
+
+
+
+
+
+
+
+
+{/* <div className={Styles.Poultry}>
+<h1 className={Styles.Title}>Chicken</h1>
+<div className={Styles.Rack}>
+  <div className={Styles.Filtering}>
+    <input placeholder="Search.." type="text" className="Search" />
+
+    <button onClick={loadMore}>More</button>
+  </div>
+  <div className={Styles.Product_Tray}>
+    {productData
+      .slice(0, visible)
+      .filter((data) => {
+        return searchProduct.toLocaleLowerCase() === ""
+          ? data
+          : data.Title.toLocaleLowerCase().includes(searchProduct);
+      })
+      .map((product) => {
+        const { id, Title, Image, Description, Category } = product;
+
+        return (
+          <div className={Styles.Card} key={id}>
+            <div className={Styles.Display_Image_Container}>
+              <h3 className={Styles.Card_Title}>{Title}</h3>
+              <img src="" alt="" className={Styles.Display_image} />
+            </div>
+
+            <div className={Styles.Card_Details}>
+              <p className={Styles.Description}>{Description}</p>
+
+              <div className={Styles.Recipe__Button}>
+                <a href="#" className={Styles.RecipeLink}>
+                  Recipe
+                </a>
+
+                <button className={Styles.Order}>Order</button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+  </div>
+</div>
+</div> */}
--- src/Components/Racks/Poultry/Poultry.jsx
+++ src/Components/Racks/Poultry/Poultry.jsx
@@ -1,56 +1,85 @@
 import React, { useState, useEffect } from "react";
 import Styles from "./Styles/Poultry.module.css";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+// import "swiper/css/effect-fade ";
+
 import Chicken from "../../Cards/Chicken";
 
 const Poultry = () => {
   const [productData, setProductData] = useState(Chicken);
   const [searchProduct, setSearchProduct] = useState("");
-  const [visible, setVisible] = useState(3);
+  // const [visible, setVisible] = useState(6);
 
-  const loadMore = () => {
-    setVisible((prev) => prev + 3)
-  }
+  // const loadMore = () => {
+  //   setVisible((prev) => prev + 3);
+  // };
 
   return (
-    <div className={Styles.Poultry}>
-      <h1 className={Styles.Title}>Chicken</h1>
-      <div className={Styles.Rack}>
-        <div className={Styles.Filtering}>
-          <input placeholder="Search.." type="text" className="Search" />
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+      speed={800}
+      modules={[Navigation]}
+      // slidesPerView={1}
+      loop
+      className={Styles.Swiper}
+    >
+      <div className={Styles.Product_Tray}>
+        {productData
+          // .slice(0, visible)
+          .filter((data) => {
+            return searchProduct.toLocaleLowerCase() === ""
+              ? data
+              : data.Title.toLocaleLowerCase().includes(searchProduct);
+          })
+          .map((product) => {
+            const { id, Title, Image, Description, Category } = product;
 
-          <button onClick={loadMore}>More</button>
-        </div>
-        <div className={Styles.Product_Tray}>
-          {productData.slice(0, visible)
-            .filter((data) => {
-              return searchProduct.toLocaleLowerCase() === ""
-                ? data
-                : data.Title.toLocaleLowerCase().includes(searchProduct);
-            })
-            .map((product) => {
-              const { id, Title, Image, Description, Category } = product;
+            return (
+              <SwiperSlide className={Styles.Slide}>
+                <div className={Styles.Card} key={id}>
+                  <div className={Styles.Display_Image_Container}>
+                    <h3 className={Styles.Card_Title}>{Title}</h3>
+                    <img src="" alt="" className={Styles.Display_image} />
+                  </div>
 
-              return <div className={Styles.Card} key={id}>
-                <h3 className={Styles.Card_Title}>{Title}</h3>
-                <div className={Styles.Display_Image_Container}>
-                  <img src="" alt="" className={Styles.Display_image} />
+                  <div className={Styles.Card_Details}>
+                    <p className={Styles.Description}>{Description}</p>
+
+                    <div className={Styles.Recipe__Button}>
+                      <a href="#" className={Styles.RecipeLink}>
+                        Recipe
+                      </a>
+
+                      <button className={Styles.Order}>Order</button>
+                    </div>
+                  </div>
                 </div>
-
-                <p className={Styles.Description}>{Description}</p>
-
-                <div className={Styles.Recipe__Button}>
-                  <a href="#" className={Styles.RecipeLink}>Recipe</a>
-
-                  <button className={Styles.Order}>
-                    Order
-                  </button>
-                </div>
-              </div>;
-            })}
-        </div>
+              </SwiperSlide>
+            );
+          })}
       </div>
-    </div>
+      {/* <SwiperSlide className={Styles.Slide}>
+        
+      </SwiperSlide>
+      <SwiperSlide className={Styles.Slide}></SwiperSlide>
+      <SwiperSlide className={Styles.Slide}></SwiperSlide> */}
+      ...
+    </Swiper>
   );
 };
 
