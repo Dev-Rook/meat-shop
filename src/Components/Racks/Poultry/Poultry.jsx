a6e43b6724faeb946974b8c6139325290fc3888f
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-09-04 12:23:46 -0500

    Mapping data begins

--- src/Components/Racks/Poultry/Poultry.jsx
+++ src/Components/Racks/Poultry/Poultry.jsx
@@ -1,10 +1,36 @@
 import React, { useState, useEffect } from "react";
 import Styles from "./Styles/Poultry.module.css";
 
-const Poultry = () => {
-  return (
-    <div className={Styles.Poultry}>Poultry</div>
-  )
-}
+import Chicken from "../../Cards/Chickesn";
 
-export default Poultry
+const Poultry = () => {
+  const [productData, setProductData] = useState(Chicken);
+  const [searchProduct, setSearchProduct] = useState("");
+  return (
+    <div className={Styles.Poultry}>
+      <div className={Styles.Rack}>
+        <h1 className={Styles.Title}>Chicken</h1>
+
+        <div className={Styles.Product_Tray}>
+          <div className={Styles.Filtering}>
+            <input placeholder="Search.." type="text" className="Search" />
+          </div>
+
+          {productData.filter((data) => {
+            return searchProduct.toLocaleLowerCase() === ""
+              ? data
+              : data.title.toLocaleLowerCase().includes(searchProduct);
+          }).map((product) => {
+            const {id, Title, Image, Description, Category} = product;
+
+            return (
+                
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Poultry;
--- src/Views/_Landing/Landing.jsx
+++ src/Views/_Landing/Landing.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import Styles from "./Styles/Landing.module.css";
 
 const Landing = () => {
+
+
   return (
     <div className={Styles.Landing}>
-        
+     
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
