602176711b3a1eb837bd0a3b9864cdac1b23e9ce
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-09-04 14:24:27 -0500

    Styling first mapped data

--- src/Components/Racks/Poultry/Poultry.jsx
+++ src/Components/Racks/Poultry/Poultry.jsx
@@ -1,32 +1,53 @@
 import React, { useState, useEffect } from "react";
 import Styles from "./Styles/Poultry.module.css";
 
-import Chicken from "../../Cards/Chickesn";
+import Chicken from "../../Cards/Chicken";
 
 const Poultry = () => {
   const [productData, setProductData] = useState(Chicken);
   const [searchProduct, setSearchProduct] = useState("");
+  const [visible, setVisible] = useState(3);
+
+  const loadMore = () => {
+    setVisible((prev) => prev + 3)
+  }
+
   return (
     <div className={Styles.Poultry}>
+      <h1 className={Styles.Title}>Chicken</h1>
       <div className={Styles.Rack}>
-        <h1 className={Styles.Title}>Chicken</h1>
+        <div className={Styles.Filtering}>
+          <input placeholder="Search.." type="text" className="Search" />
 
+          <button onClick={loadMore}>More</button>
+        </div>
         <div className={Styles.Product_Tray}>
-          <div className={Styles.Filtering}>
-            <input placeholder="Search.." type="text" className="Search" />
-          </div>
+          {productData.slice(0, visible)
+            .filter((data) => {
+              return searchProduct.toLocaleLowerCase() === ""
+                ? data
+                : data.Title.toLocaleLowerCase().includes(searchProduct);
+            })
+            .map((product) => {
+              const { id, Title, Image, Description, Category } = product;
 
-          {productData.filter((data) => {
-            return searchProduct.toLocaleLowerCase() === ""
-              ? data
-              : data.title.toLocaleLowerCase().includes(searchProduct);
-          }).map((product) => {
-            const {id, Title, Image, Description, Category} = product;
+              return <div className={Styles.Card} key={id}>
+                <h3 className={Styles.Card_Title}>{Title}</h3>
+                <div className={Styles.Display_Image_Container}>
+                  <img src="" alt="" className={Styles.Display_image} />
+                </div>
 
-            return (
-                
-            )
-          })}
+                <p className={Styles.Description}>{Description}</p>
+
+                <div className={Styles.Recipe__Button}>
+                  <a href="#" className={Styles.RecipeLink}>Recipe</a>
+
+                  <button className={Styles.Order}>
+                    Order
+                  </button>
+                </div>
+              </div>;
+            })}
         </div>
       </div>
     </div>
--- src/Views/_Landing/Landing.jsx
+++ src/Views/_Landing/Landing.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Styles from "./Styles/Landing.module.css";
 
+import Poultry from "../../Components/Racks/Poultry/Poultry";
+
 const Landing = () => {
-
-
-  return (
-    <div className={Styles.Landing}>
-     
-    </div>
-  );
+  return <div className={Styles.Landing}>
+    <Poultry />
+  </div>;
 };
 
 export default Landing;
