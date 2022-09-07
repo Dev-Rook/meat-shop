9deb9dd54cf26233f2c41c9e2bd9b33dfb37bc15
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-09-06 20:02:57 -0500

    Copy side menu from previous project.

--- src/Components/_Nav/Nav.jsx
+++ src/Components/_Nav/Nav.jsx
@@ -4,10 +4,21 @@ import Styles from "./Styles/Nav.module.css";
 
 // Material UI Icons Start
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import SearchIcon from "@mui/icons-material/Search";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import TwitterIcon from "@mui/icons-material/Twitter";
 // Material UI Icons End
 
 const Nav = () => {
   const [Nav, setNav] = useState(false);
+  const [toogleSideBar, setToogleSideBar] = useState(false);
+
+  const toggleSB = () => {
+    setToogleSideBar((prev) => !prev);
+  };
 
 
   const hideNav = () => {
@@ -53,8 +64,93 @@ const Nav = () => {
           </Link>
         </li>
 
-        <MenuIcon sx={{fontSize: 40}} className={Styles.Menu_Icon} />
+        <MenuIcon sx={{fontSize: 40}} onClick={toggleSB} className={Styles.Menu_Icon} />
       </ul>
+
+
+
+
+      <div
+        className={`${Styles.Mobile__Menu} ${
+          toogleSideBar ? Styles.ToggleMenu : ""
+        }`}
+      >
+        <CloseIcon className={Styles.CloseIcon} onClick={toggleSB} />
+        <LocalMallIcon className={Styles.MerchIcon} />
+
+        <h1 className={Styles.Mobile_Brand}>Marina Palms</h1>
+
+        <ul className={Styles.MobileLinks_Container}>
+          <li className={Styles.MobileLink_Item}>
+            <a href="#" className={Styles.MobileLink}>
+              Home
+            </a>
+          </li>
+          <li className={Styles.MobileLink_Item}>
+            <a href="#" className={Styles.MobileLink}>
+              Visit
+            </a>
+          </li>
+          <li className={Styles.MobileLink_Item}>
+            <a href="#" className={Styles.MobileLink}>
+              Sailing
+            </a>
+          </li>
+          <li className={Styles.MobileLink_Item}>
+            <a href="#" className={Styles.MobileLink}>
+              Fishing
+            </a>
+          </li>
+          <li className={Styles.MobileLink_Item}>
+            <a href="#" className={Styles.MobileLink}>
+              Contact
+            </a>
+          </li>
+
+          <div className={Styles.Mobile_Search_Container}>
+            <SearchIcon className={Styles.SearchIcon} />
+            <input
+              placeholder="Search..."
+              type="text"
+              className={Styles.Search}
+            />
+          </div>
+
+          <div className={Styles.Social_Icon_Bar}>
+            <div className={Styles.Social_Icon}>
+              {/* <img
+                src={require("../../Assets/Icons/Twitter-Icon.png")}
+                alt=""
+                className={Styles.SocialIcon}
+              /> */}
+            </div>
+            <div className={Styles.Social_Icon}>
+              {" "}
+              {/* <img
+                src={require("../../Assets/Icons/Facebook-Icon.png")}
+                alt=""
+                className={Styles.SocialIcon}
+              /> */}
+            </div>
+            <div className={Styles.Social_Icon}>
+              {" "}
+              {/* <img
+                src={require("../../Assets/Icons/Instagram-Icon.png")}
+                alt=""
+                className={Styles.SocialIcon}
+              /> */}
+            </div>
+            <div className={Styles.Social_Icon}>
+              {" "}
+              {/* <img
+                src={require("../../Assets/Icons/YouTube-Icon.png")}
+                alt=""
+                className={Styles.SocialIcon}
+              /> */}
+            </div>
+          </div>
+        </ul>
+      </div>
     </div>
   );
 };
